adaf7875a52102471b5cee3824b79faadd2214ee
Muskansingh21 <contact.muskansingh01@gmail.com>
2026-05-10 21:59:33 +0530

    issue resolve for email-validation

--- src/validation/auth/Register.js
+++ src/validation/auth/Register.js
@@ -2,7 +2,7 @@ import { number, object, string } from 'yup';
 
 export const registerValidationSchema = object({
   full_name: string().required('Full name is required field'),
-  email: string().lowercase().required('Email is reqired field'),
+  email: string().email().required('Email is reqired field'),
   phone: number().min(10).required('Phone no. is must'),
   password: string().min(5).max(14).required('Password must be strong'),
 });
